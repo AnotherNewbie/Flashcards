abd91c263eb1487989c861d57fa01ba149064cf8
Michael Holthausen <mgholthausen@gmail.com>
2022-01-04 23:48:02 -0500

    fixed readDeck

--- src/Layout/Deck.js
+++ src/Layout/Deck.js
@@ -1,45 +1,36 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
 import { readDeck } from "../utils/api";
 
 import ErrorMessage from "./ErrorMessage";
 
 export default function Deck() {
-    const {deckId} = useParams();    
-    const [deck, setDeck] = useState({});
-    const [error, setError] = useState(undefined);
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState({cards: []});
+  const [error, setError] = useState(undefined);
 
-    // useEffect(()=> {
-    //     const ac = new AbortController();        
-    //     readDeck(deckId, ac.signal).then(setDeck).catch(setError);     
+  useEffect(() => {
+    const ac = new AbortController();
+    readDeck(deckId, ac.signal)
+      .then(d => {
+          console.log(d);
+          setDeck(d);
+          return () => ac.abort()
+        }).catch((error) => setError(error));
+  }, []);
 
-    //     return () => ac.abort();
-    // },[]);
+  if (error) {
+    return <ErrorMessage error={error} />;
+  }
+ 
+  const deckList = deck.cards.map((card) => (
+    <div key={card.id}>
+      <p>{card.id}</p>
+      <p>Front: {card.front}</p>
+      <p>Back: {card.back}</p>
+    </div>
+  ));
 
-    // if(error) {
-    //     return <ErrorMessage error={error} />;
-    // }
-    
-    
-    // useEffect(() => {
-    //     async function loadCards() {
-    //         const response = await readDeck(deckId);                      
-    //         setDeck(response);            
-    //     }        
-    //     loadCards();
-    // },[]);
-
-    
-    // const deckList = deck.cards.map((card) => (
-    //      <div>
-    //          <p>{card.id}</p><p>Front: {card.front}</p><p>Back: {card.back}</p>
-    //      </div>
-    // ));
-
-    return(
-        <>
-        {/* {deckList} */}        
-        </>
-    )
+  return <>{deckList}</>;
 }
