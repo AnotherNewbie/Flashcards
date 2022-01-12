80fe5e7310758cb6a0bd44f4d9bf50bbe773118b
Michael Holthausen <mgholthausen@gmail.com>
2022-01-12 15:03:34 -0500

    moved AddCard away from using CardForm

--- src/Layout/AddCard.js
+++ src/Layout/AddCard.js
@@ -1,6 +1,12 @@
 import React from "react";
 import CardForm from "./CardForm";
 
+const [front, setFront] = useState();
+const [back, setBack] = useState();
+
+const handleFrontChange = (event) => setFront(event.target.value);
+const handleBackChange = (event) => setBack(event.target.value);    
+
 export function AddCardHandler(){
     
 }
@@ -9,7 +15,17 @@ export default function AddCard() {
 
     return(
         <>
-            <CardForm />
+            {/* <CardForm /> */}
+            <form>
+               <label htmlFor="front">
+                Enter front of card:
+                <textarea id="front" cols="50" rows="5" maxlength="250" name="front" onChange={handleFrontChange} value={front}>{front}</textarea>
+               </label>
+               <label htmlFor="back">
+                Enter back of card:
+                <textarea id="back" cols="50" rows="5" maxlength="250" name="back" onChange={handleBackChange} value={back} >{back}</textarea>
+               </label>
+            </form> 
         </>
     )
 }
