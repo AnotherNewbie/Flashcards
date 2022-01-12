df96eaee720fa1588346719d76e05d1b08fd44a0
Michael Holthausen <mgholthausen@gmail.com>
2022-01-12 15:51:07 -0500

    fixed textarea in EditCard and AddCard

--- src/Layout/AddCard.js
+++ src/Layout/AddCard.js
@@ -1,11 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import CardForm from "./CardForm";
 
-const [front, setFront] = useState();
-const [back, setBack] = useState();
-
-const handleFrontChange = (event) => setFront(event.target.value);
-const handleBackChange = (event) => setBack(event.target.value);    
 
 export function AddCardHandler(){
     
@@ -13,17 +8,25 @@ export function AddCardHandler(){
 
 export default function AddCard() {
 
+    const [front, setFront] = useState();
+    const [back, setBack] = useState();
+
+
+    const handleFrontChange = (event) => setFront(event.target.value);
+    const handleBackChange = (event) => setBack(event.target.value);    
+
+
     return(
         <>
             {/* <CardForm /> */}
             <form>
                <label htmlFor="front">
                 Enter front of card:
-                <textarea id="front" cols="50" rows="5" maxlength="250" name="front" onChange={handleFrontChange} value={front}>{front}</textarea>
+                <textarea id="front" cols="50" rows="5" maxlength="250" name="front" onChange={handleFrontChange} value={front}></textarea>
                </label>
                <label htmlFor="back">
                 Enter back of card:
-                <textarea id="back" cols="50" rows="5" maxlength="250" name="back" onChange={handleBackChange} value={back} >{back}</textarea>
+                <textarea id="back" cols="50" rows="5" maxlength="250" name="back" onChange={handleBackChange} value={back}></textarea>
                </label>
             </form> 
         </>
--- src/Layout/EditCard.js
+++ src/Layout/EditCard.js
@@ -1,56 +1,72 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import CardForm from "./CardForm";
 import { readCard } from "../utils/api";
 import ErrorMessage from "./ErrorMessage";
 import { useParams } from "react-router-dom";
 
 export default function EditCard(props) {
-    // const {cardId} = props;
-    const [card, setCard] = useState({id:"",front:"", back:"", deckId:""});
-    const ac = new AbortController();
-    const [error, setError] = useState(undefined);
-    const {cardId, deckId} = useParams();
-    const [front, setFront] = useState();
-    const [back, setBack] = useState();
+  // const {cardId} = props;
+  const [card, setCard] = useState({ id: "", front: "", back: "", deckId: "" });
+  const ac = new AbortController();
+  const [error, setError] = useState(undefined);
+  const { cardId, deckId } = useParams();
+  const [front, setFront] = useState();
+  const [back, setBack] = useState();
 
-    const handleFrontChange = (event) => setFront(event.target.value);
-    const handleBackChange = (event) => setBack(event.target.value);    
+  const handleFrontChange = (event) => setFront(event.target.value);
+  const handleBackChange = (event) => setBack(event.target.value);
 
-    console.log(cardId);
+  useEffect(() => {
+    console.log(`cardId ${cardId}`);
+    if (!cardId) {
+      return;
+    }
+    readCard(cardId, ac.signal)
+      .then((c) => {
+        console.log(`updating card: ${JSON.stringify(c, null, 4)}`);
+        setCard(c);
+        setFront(card.front);
+        setBack(card.back);
+        return () => ac.abort();
+      })
+      .catch((error) => setError(error));
+  }, [cardId]);
 
-    useEffect(() => {
-        console.log(`cardId ${cardId}`);
-        if (!cardId) {      
-          return;
-        }
-        readCard(cardId, ac.signal)
-          .then((c) => {
-            console.log(`updating card: ${JSON.stringify(c, null, 4)}`);
-            setCard(c);
-            setFront(card.front);
-            setBack(card.back);
-            return () => ac.abort();
-          })
-          .catch((error) => setError(error));
-      },[cardId]);
+  if (error) {
+    return <ErrorMessage error={error} />;
+  }
 
-      if (error) {
-        return <ErrorMessage error={error} />;
-      }      
-      
-    return(
-        <>
-            {/* <CardForm cardId={cardId} frontIN={card.front} backIN={card.back} deckId={deckId}/> */}
-            <form>
-               <label htmlFor="front">
-                Enter front of card:
-                <textarea id="front" cols="50" rows="5" maxlength="250" name="front" onChange={handleFrontChange} value={front}>{front}</textarea>
-               </label>
-               <label htmlFor="back">
-                Enter back of card:
-                <textarea id="back" cols="50" rows="5" maxlength="250" name="back" onChange={handleBackChange} value={back} >{back}</textarea>
-               </label>
-            </form> 
-        </>
-    )
+  return (
+    <>
+      {/* <CardForm cardId={cardId} frontIN={card.front} backIN={card.back} deckId={deckId}/> */}
+      <form>
+        <label htmlFor="front">
+          Enter front of card:
+          <textarea
+            id="front"
+            cols="50"
+            rows="5"
+            maxLength="250"
+            name="front"
+            onChange={handleFrontChange}
+            value={card.front}
+          >            
+          </textarea>
+        </label>
+        <label htmlFor="back">
+          Enter back of card:
+          <textarea
+            id="back"
+            cols="50"
+            rows="5"
+            maxLength="250"
+            name="back"
+            onChange={handleBackChange}
+            value={card.back}
+          >            
+          </textarea>
+        </label>
+      </form>
+    </>
+  );
 }
